032e9833e6098de371986e266e32a8a35aeba60c
Minh Nam <duongminhnam99@gmail.com>
2022-02-11 09:17:44 +0700

    [Update] boid in group

--- multithreading/boids/main.go
+++ multithreading/boids/main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"image/color"
 	"log"
+
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 const (
 	screenWidth, screenHeight = 640, 360
-	boidCount = 500
+	boidCount                 = 500
+	viewRadius                = 13
+	adjRate                   = 0.015
 )
 
 var (
-	green = color.RGBA{10, 255, 50, 255}
-	boids [boidCount]*Boid
+	green   = color.RGBA{10, 255, 50, 255}
+	boids   [boidCount]*Boid
+	boidMap [screenWidth + 1][screenHeight + 1]int
 )
 
 type Game struct{}
@@ -36,6 +40,12 @@ func (g *Game) Layout(_, _ int) (w, h int) {
 }
 
 func main() {
+	// for i, row := range boidMap {
+	// 	for j := range row {
+	// 		boidMap[i][j] = -1
+	// 	}
+	// }
+
 	for i := 0; i < boidCount; i++ {
 		createBoid(i)
 	}
