b1de636b7ff84bc3b534feedc3e97942c5aa103e
Minh Nam <duongminhnam99@gmail.com>
2022-02-15 23:21:26 +0700

    [Fix] train laucher

--- deadlocks_train/launcher.go
+++ deadlocks_train/launcher.go
@@ -5,12 +5,13 @@ import (
 	"sync"
 
 	"github.com/duongnam99/go-multithread-practice/deadlocks_train/common"
+	"github.com/duongnam99/go-multithread-practice/deadlocks_train/hierarchy"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 var (
-	trains        [4]*Train
-	intersections [4]*Intersection
+	trains        [4]*common.Train
+	intersections [4]*common.Intersection
 )
 
 const trainLength = 70
@@ -40,16 +41,16 @@ func main() {
 		intersections[i] = &common.Intersection{Id: i, Mutex: sync.Mutex{}, LockedBy: -1}
 	}
 
-	go MoveTrain(trains[0], 300, []*common.Crossing{{Position: 125, Intersection: intersections[0]},
+	go hierarchy.MoveTrain(trains[0], 300, []*common.Crossing{{Position: 125, Intersection: intersections[0]},
 		{Position: 175, Intersection: intersections[1]}})
 
-	go MoveTrain(trains[1], 300, []*common.Crossing{{Position: 125, Intersection: intersections[1]},
+	go hierarchy.MoveTrain(trains[1], 300, []*common.Crossing{{Position: 125, Intersection: intersections[1]},
 		{Position: 175, Intersection: intersections[2]}})
 
-	go MoveTrain(trains[2], 300, []*common.Crossing{{Position: 125, Intersection: intersections[2]},
+	go hierarchy.MoveTrain(trains[2], 300, []*common.Crossing{{Position: 125, Intersection: intersections[2]},
 		{Position: 175, Intersection: intersections[3]}})
 
-	go MoveTrain(trains[3], 300, []*common.Crossing{{Position: 125, Intersection: intersections[3]},
+	go hierarchy.MoveTrain(trains[3], 300, []*common.Crossing{{Position: 125, Intersection: intersections[3]},
 		{Position: 175, Intersection: intersections[0]}})
 
 	ebiten.SetWindowSize(320*3, 320*3)
