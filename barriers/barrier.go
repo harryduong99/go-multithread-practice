2e7f8e944018cae5b793c8616db5f0aae429100f
Minh Nam <duongminhnam99@gmail.com>
2022-02-18 18:35:22 +0700

    [Add] coments for barrier code

--- barriers/barrier.go
+++ barriers/barrier.go
@@ -12,17 +12,17 @@ type Barrier struct {
 func NewBarrier(size int) *Barrier {
 	lockToUse := &sync.Mutex{}
 	condToUse := sync.NewCond(lockToUse)
-	return &Barrier{size, size, lockToUse, condToUse}
+	return &Barrier{size, size, lockToUse, condToUse} // initial, count == size of thread
 }
 
 func (b *Barrier) Wait() {
-	b.mutex.Lock()
-	b.count -= 1
-	if b.count == 0 {
+	b.mutex.Lock()    // safely modify the count
+	b.count -= 1      // 1 more thread is waiting
+	if b.count == 0 { // if all if the threads have called wait function
 		b.count = b.total
 		b.cond.Broadcast()
 	} else {
-		b.cond.Wait()
+		b.cond.Wait() // keep wating
 	}
 	b.mutex.Unlock()
 }
